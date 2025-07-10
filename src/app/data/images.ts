431f1536cc148dcd5a1872be225d6da80c5859d1
dmsumon2020 <dmsumon2020@gmail.com>
2025-07-10 23:10:46 +0600

    first commit

---
+++ src/app/data/images.ts
@@ -0,0 +1,13 @@
+const images = [
+  "https://picsum.photos/id/1011/500/700",
+  "https://picsum.photos/id/1015/500/600",
+  "https://picsum.photos/id/1025/500/800",
+  "https://picsum.photos/id/1005/500/500",
+  "https://picsum.photos/id/1041/500/700",
+  "https://picsum.photos/id/1060/500/600",
+  "https://picsum.photos/id/1084/500/750",
+  "https://picsum.photos/id/1032/500/650",
+  "https://picsum.photos/id/1024/500/500",
+];
+
+export default images;
